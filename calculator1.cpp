a47e5612adc616451e07961eb4f99f63a38c9cb3
Nikita <57573387+gylesis@users.noreply.github.com>
2020-01-03 22:02:29 +0200

    Add files via upload

---
+++ calculator1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, res;
+	string num;
+	cout << "Choose operation for numbers: multiplication/subtraction/addition/division";
+	cin >> num;
+	cout << "Write down these numbers";
+	cin >> a, b;
+	if (num == "multiplication") {
+		res = a * b
+	}
+
+}
