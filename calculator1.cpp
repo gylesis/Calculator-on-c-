148f6b40bf67d4df04ca98eb61fe049fed7cfcc3
Nikita <57573387+gylesis@users.noreply.github.com>
2020-01-03 22:32:56 +0200

    Update calculator1.cpp

--- calculator1.cpp
+++ calculator1.cpp
@@ -4,12 +4,29 @@ int main()
 {
 	int a, b, res;
 	string num;
-	cout << "Choose operation for numbers: multiplication/subtraction/addition/division";
+	cout << "Choose operation for numbers: multiplication/subtraction/addition/division" << endl;
 	cin >> num;
-	cout << "Write down these numbers";
-	cin >> a, b;
-	if (num == "multiplication") {
-		res = a * b
+	cout << "Write down these numbers" << endl;
+	cin >> a;
+	cin >> b;
+	if (num == "mult") {
+		res = a * b;
+		cout << "Result is" << endl;
+		cout << res;
 	}
-
+	else if (num == "subtraction") {
+		res = a - b;
+		cout << "Result is" << endl;
+		cout << res;
+	}
+	else if (num == "addition") {
+		res = a + b;
+		cout << "Result is" << endl;
+		cout << res;
+	}
+	else if (num == "division") {
+		res = a / b;
+		cout << "Result is" << endl;
+		cout << res;
+	}
 }
